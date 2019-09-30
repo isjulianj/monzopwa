87a44dc1f04798fb0263dd04039e5e472b66804f
isjulianJaywing <julian.jackson@jaywing.com>
2019-09-30 21:42:05 +0100

    tweaked header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
+
 export const Header: React.FC = (props: string) => {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        <img
-          src="//placehold.it/30x30"
-          width="30"
-          height="30"
-          class="d-inline-block align-top"
-          alt=""
-        />
-        &nbsp; Monzo PWA
-      </a>
-    </nav>
+    <header className="Header container">
+      <nav className="navbar navbar-light bg-light Header__navigation ">
+        <a className="navbar-brand" href="#">
+          <img
+            src="//placehold.it/30x30"
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt=""
+          />
+          &nbsp; Monzo PWA
+        </a>
+      </nav>
+    </header>
   );
 };
--- src/components/LogMeIn.tsx
+++ src/components/LogMeIn.tsx
@@ -1,14 +1,33 @@
-import * as React from 'react';
+import * as React from "react";
 
 export interface IProps {
   compiler: string;
   framework: string;
 }
 
+const jumbotronBg = {
+  background:
+    "url('https://developers.monzo.com/static/media/developers-bg.7a68fb39.jpg')"
+};
+
 export const LogMeIn: React.FC<IProps> = props => {
   return (
     <div className="container">
-      <h1>Log into your account!</h1>;
+      <div
+        className="Header__hero jumbotron jumbotron-fluid d-flex text-white"
+        style={jumbotronBg}
+      >
+        <div className="container">
+          <div className="p-1">
+            <h1>Log into your account!</h1>
+          </div>
+          <div className="p-1">
+            <button className="btn btn-primary">
+              Sign in with your monzo account
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
